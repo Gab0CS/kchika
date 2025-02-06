7ea354b6d5e44b58b36a25d09e0c37cf3144e9d4
Gab0CS <gabocanseco@gmail.com>
2025-02-06 16:29:05 -0600

    first commit

---
+++ src/main/java/com/gabo/kchika/KchikaApplication.java
@@ -0,0 +1,13 @@
+package com.gabo.kchika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KchikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KchikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabo/kchika/KchikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabo.kchika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KchikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
